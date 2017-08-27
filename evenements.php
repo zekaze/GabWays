653f2cb50f9593058526784463d58333ff5dba53
Alexandre Kaze <zekaze@users.noreply.github.com>
2017-08-27 02:01:04 +0100

    Updates de la page événements

--- evenements.php
+++ evenements.php
@@ -1,9 +1,50 @@
 <?php include_once "layout/head.php";?>
 <?php include_once "layout/header-2.php";?>
 <div class="page-banner" style="background-image: url('img/grey-bg.jpg')">
-    <div class="container">
-
+    <div class="container-fluid">
+        <div class="section-slider">
+            <?php $dates = array(
+                '28',
+                array('01','05'),
+                array('15','16'),
+                '17',
+                '09',
+                '21',
+                array('10','11')
+            );?>
+            <div class="event_list">
+                <?php foreach ($dates as $date):?>
+                    <a href="#">
+                        <div class="article">
+                            <div class="article_picture_frame">
+                                <img src="holder.js/560x400" alt="Holder">
+                            </div>
+                            <?php if(is_array($date)):?>
+                                <div class="event_date double">
+                                    <div class="date">
+                                        <span class="day"><?php echo $date[0];?></span>
+                                        <span class="month">Aoû</span>
+                                    </div>
+                                    <div class="date">
+                                        <span class="day"><?php echo $date[1];?></span>
+                                        <span class="month">Aoû</span>
+                                    </div>
+                                </div>
+                            <?php else:?>
+                                <div class="event_date single">
+                                    <div class="date">
+                                        <span class="day"><?php echo $date;?></span>
+                                        <span class="month">Aoû</span>
+                                    </div>
+                                </div>
+                            <?php endif;?>
+                        </div>
+                    </a>
+                <?php endforeach;?>
+            </div>
+        </div>
     </div>
+
 </div>
 <!--end of page-banner-->
 
@@ -53,9 +94,10 @@
                         array('10','11')
                     );
 
-                    $counter = 0;
+                    $i = 0;
+                    $j = 0;
                     foreach ($months as $month):
-                        $counter++;
+                        $i++;
                     ?>
                     
                     <div class="section section-slider">
@@ -70,6 +112,7 @@
                         <div class="section-body">
                             <div class="event_list">
                                 <?php foreach ($dates as $date):?>
+                                    <?php $j++;?>
                                     <a href="#">
                                         <div class="article">
                                             <div class="article_picture_frame">
@@ -94,7 +137,7 @@
                                                     </div>
                                                 </div>
                                             <?php endif;?>
-                                            <div class="article_title">Titre de l'événement <?php echo $counter;?></div>
+                                            <div class="article_title">Titre de l'événement <?php echo $j;?></div>
                                         </div>
                                     </a>
                                 <?php endforeach;?>
@@ -102,7 +145,7 @@
                         </div>
                     </div>
                     <?php
-                    if(!($counter % 2)): ?>
+                    if(!($i % 2)): ?>
                         <div class="publicite">
                             <div class="placeholder">
                                 Publicité
