8fc96df6b3c7d37d7734ce86861b170fd959725d
Alexandre Kaze <zekaze@users.noreply.github.com>
2017-09-10 23:15:54 +0100

    Ajout de la page 404

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php include_once "layout/head.php";?>
+<?php include_once "layout/header.php";?>
+
+    <style>
+        body{
+            background: #ddd;
+        }
+    </style>
+
+<div class="body bg-cover page-404">
+    <div class="container">
+        <div class="content">
+            <div class="h1">Oops!</div>
+            <div class="h3">Il semblerait que nous n'arrivions pas à localiser la page que vous recherchez. Pendant que nos agents sont sur le terrain à la chercher nous vous invitons à</div>
+            <a href="#" class="button-large">Retourner vers la page d'accueil</a>
+        </div>
+    </div>
+</div>
+
+<?php include_once "layout/footer.php";?>
