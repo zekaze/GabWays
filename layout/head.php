57dcd7d19eb87b73e2fc1a69d3e0328148549602
Alexandre Kaze <zekaze@users.noreply.github.com>
2017-09-12 22:50:02 +0100

    Ajout de la couleur

--- layout/head.php
+++ layout/head.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="theme-color" content="#b9b9b9">
+    <meta name="theme-color" content="#118c72">
     <link rel="shortcut icon" type="image/x-icon" href="favicon.png">
     <title>Bienvenue sur GabWays</title>
 
