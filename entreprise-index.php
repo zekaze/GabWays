74ab0e7e99caf96f8271b41d77c6183d613071a9
Alexandre Kaze <zekaze@users.noreply.github.com>
2017-09-26 02:54:48 +0100

    Page d'administration

--- entreprise-index.php
+++ entreprise-index.php
@@ -6,16 +6,14 @@
     <div class="body admin">
         <div class="admin-sidebar">
                 <div class="header">
-                    <div class="dashboard-label"><i class="fa fa-dashboard fa-lg"></i>Admin GabWays</div>
+                    <div class="dashboard-label"><img src="img/logo.png" height="40"></div>
                 </div>
 
                 <ul class="sidebar-menu">
-                    <!--<li><a href="entreprise-index.php"><i class="fa fa-home fa-lg"></i>Accueil administration</a></li>-->
-                    <!--<li><a href="index.php"><i class="fa fa-home fa-lg"></i>Accueil GabWays</a></li>-->
                     <li><a href="#"><i class="fa fa-user fa-lg"></i>Mon profil</a></li>
                     <li><a href="#"><i class="fa fa-clipboard fa-lg"></i>Fiche entreprise</a></li>
                     <li><a href="#"><i class="fa fa-photo fa-lg"></i>Media</a></li>
-                    <li><a href="#"><i class="fa fa-dashboard fa-lg"></i>Mes événements</a></li>
+                    <li><a href="#"><i class="fa fa-calendar fa-lg"></i>Mes événements</a></li>
                     <li><a href="#"><i class="fa fa-bullhorn fa-lg"></i>Mes publicités</a></li>
                 </ul>
         </div>
