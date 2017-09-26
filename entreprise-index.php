e5c0e01fe2bd8c62cd07013a0d052d966b9bc788
Alexandre Kaze <zekaze@users.noreply.github.com>
2017-09-26 02:17:09 +0100

    Page d'administration

---
+++ entreprise-index.php
@@ -0,0 +1,30 @@
+<?php include_once "layout/head.php";?>
+<?php /*include_once "layout/admin-header.php";*/?>
+
+
+<?php include_once "data.php";?>
+    <div class="body admin">
+        <div class="admin-sidebar">
+                <div class="header">
+                    <div class="dashboard-label"><i class="fa fa-dashboard fa-lg"></i>Admin GabWays</div>
+                </div>
+
+                <ul class="sidebar-menu">
+                    <li><a href="entreprise-index.php"><i class="fa fa-home fa-lg"></i>Accueil administration</a></li>
+                    <li><a href="index.php"><i class="fa fa-home fa-lg"></i>Accueil GabWays</a></li>
+                    <li><a href="#"><i class="fa fa-user fa-lg"></i>Mon profil</a></li>
+                    <li><a href="#"><i class="fa fa-clipboard fa-lg"></i>Fiche entreprise</a></li>
+                    <li><a href="#"><i class="fa fa-photo fa-lg"></i>Media</a></li>
+                    <li><a href="#"><i class="fa fa-dashboard fa-lg"></i>Mes événements</a></li>
+                    <li><a href="#"><i class="fa fa-bullhorn fa-lg"></i>Mes publicités</a></li>
+                </ul>
+        </div>
+        <div class="body">
+            <div class="container-fluid">
+                <div>
+                    Body
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include_once "layout/footer.php";?>
---
+++ layout/admin-header.php
@@ -0,0 +1,29 @@
+<header>
+    <!--end of top nav menu-->
+    <nav class="navbar default">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a href="index.php"><i class="fa fa-home fa-lg"></i> &nbsp; Accueil GabWays</a>
+
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav"> 
+                    <li class="active"><a href="index.php">Accueil</a></li>
+                    <li><a href="categories.php">Catégories</a></li>
+                    <li><a href="evenements.php">Événements</a></li>
+                    <li><a href="about-us.php">Qui sommes nous?</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</header>
