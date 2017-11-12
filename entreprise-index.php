209c123f9b2a53ab0b6020db37955771a7ff8d0a
Alexandre Kaze <zekaze@users.noreply.github.com>
2017-11-12 23:13:42 +0100

    Dashboard

--- entreprise-index.php
+++ entreprise-index.php
@@ -7,8 +7,48 @@
         <?php include_once "layout/admin-sidebar.php";?>
         <div class="main-body">
             <div class="container-fluid">
-                <div>
-                    Body
+                <div class="admin-section">
+                    <div class="section-header">
+                        <h2>Tableau de bord</h2>
+                    </div>
+
+                    <div class="dashboard">
+                        <div class="dashboard-row">
+                            <a href="#" class="dashboard-item" style="background-color: #af1e27">
+                                <i class="fa fa-user"></i>
+                                <div class="label">
+                                    <div class="counter">237</div>
+                                    utilisateurs
+                                </div>
+                            </a>
+
+                            <a href="#" class="dashboard-item" style="background-color: #2d2d7b">
+                                <i class="fa fa-building"></i>
+                                <div class="label">
+                                    <div class="counter">312</div>
+                                    entreprises
+                                </div>
+                            </a>
+                        </div>
+                        <div class="dashboard-row">
+                            <a href="#" class="dashboard-item" style="background-color: #df8e00">
+                                <i class="fa fa-calendar"></i>
+                                <div class="label">
+                                    <div class="counter">22</div>
+                                    événements
+                                </div>
+                            </a>
+
+                            <a href="#" class="dashboard-item" style="background-color: #079615">
+                                <i class="fa fa-bullhorn"></i>
+                                <div class="label">
+                                    <div class="counter">15</div>
+                                    publicités
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
--- js/script.js
+++ js/script.js
@@ -32,12 +32,6 @@ function initMap() {
         });
     }
 
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-            infowindow.setContent(locations[i][0]);
-            infowindow.open(map, marker);
-        }
-    })(marker, i));
 
 }
 
