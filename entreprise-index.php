ff53e2f4d3b6edfbeb6850c6bbb324296a270886
Alexandre Kaze <zekaze@users.noreply.github.com>
2017-11-19 20:15:09 +0100

    Dashboard

--- entreprise-index.php
+++ entreprise-index.php
@@ -110,6 +110,116 @@
                             </div>
                         </div>
                     </div>
+                    <div class="table-responsive">
+                                <div class="panel">
+                                    <div class="panel-heading">
+                                        <h3 class="h3"><i class="fa fa-building"></i> Entreprises non validéees</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <table class="table">
+                                            <?php for($i = 0; $i <5; $i++):?>
+                                                <tr>
+                                                    <td width="70">
+                                                        <a href="#">
+                                                            <img src="<?php echo $etablissements[$i];?>" class="img-responsive" alt="">
+                                                        </a>
+                                                        </td>
+                                                    <td width="40"><?php echo $i+1;?></td>
+                                                    <td class="details_etablissement">
+                                                        <a href="#">
+                                                            <div class="nom_etablissement">Nom entreprise</div>
+                                                            <div class="quartier_etablissement">Catégorie</div>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            <?php endfor;?>
+                                        </table>
+                                        <a href="#" class="btn btn-block">Voir toutes les entreprises</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="table-responsive">
+                                <div class="panel">
+                                    <div class="panel-heading">
+                                        <h3 class="h3"><i class="fa fa-bullhorn"></i> Publicités non validées</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <table class="table">
+                                            <thead>
+                                            <th></th>
+                                            <th colspan="2">Libellé</th>
+                                            <th>Début</th>
+                                            <th>Fin</th>
+                                            </thead>
+                                            <?php for($i = 0; $i <5; $i++):?>
+                                                <tr>
+                                                    <td width="40"><?php echo $i+1;?></td>
+                                                    <td width="70"><img src="<?php echo $evenements[$i];?>" class="img-responsive" alt=""></td>
+                                                    <td class="details_etablissement">
+                                                        <a href="#">
+                                                            <div class="nom_etablissement">Libellé</div>
+                                                            <div class="quartier_etablissement">Nom de l'entreprise</div>
+
+                                                        </a>
+                                                    </td>
+                                                    <td class="details_etablissement">
+                                                        19/11/2017
+                                                    </td>
+                                                    <td class="details_etablissement">
+                                                        30/11/2017
+                                                    </td>
+                                                </tr>
+                                            <?php endfor;?>
+                                        </table>
+                                        <a href="#" class="btn btn-block">Voir toutes les publicités</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="table-responsive">
+                                <div class="panel">
+                                    <div class="panel-heading">
+                                        <h3 class="h3"><i class="fa fa-calendar"></i> Événements non validées</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <table class="table">
+                                            <thead>
+                                            <th></th>
+                                            <th colspan="2">Libellé</th>
+                                            <th>Début</th>
+                                            <th>Fin</th>
+                                            </thead>
+                                            <?php for($i = 0; $i <5; $i++):?>
+                                                <tr>
+                                                    <td width="40"><?php echo $i+1;?></td>
+                                                    <td width="70"><img src="<?php echo $evenements[$i];?>" class="img-responsive" alt=""></td>
+                                                    <td class="details_etablissement">
+                                                        <a href="#">
+                                                            <div class="nom_etablissement">Titre de l'événement</div>
+                                                            <div class="quartier_etablissement">Nom de l'entreprise</div>
+
+                                                        </a>
+                                                    </td>
+                                                    <td class="details_etablissement">
+                                                        19/11/2017
+                                                    </td>
+                                                    <td class="details_etablissement">
+                                                        30/11/2017
+                                                    </td>
+                                                </tr>
+                                            <?php endfor;?>
+                                        </table>
+                                        <a href="#" class="btn btn-block">Voir toutes les événements</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
